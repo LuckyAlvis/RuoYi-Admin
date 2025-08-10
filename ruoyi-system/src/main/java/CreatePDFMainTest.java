fdfcfd189bf01c61b01ef72039bcb0991d9e6b41
dyf_Home <252127196@qq.com>
2025-08-10 23:42:15 +0800

    feat: 格式化代码

---
+++ ruoyi-system/src/main/java/CreatePDFMainTest.java
@@ -0,0 +1,17 @@
+public class CreatePDFMainTest {
+//    public static void main(String[] args) throws Exception {
+//        Document document = new Document(PageSize.A4);
+//        //第二步，创建Writer实例
+//        PdfWriter.getInstance(document, new FileOutputStream("hello.pdf"));
+//        //创建中文字体
+//        BaseFont bfchinese = BaseFont.createFont("STSong-Light", "UniGB-UCS2-H", BaseFont.NOT_EMBEDDED);
+//        Font fontChinese = new Font(bfchinese, 12, Font.NORMAL);
+//        //第三步，打开文档
+//        document.open();
+//        //第四步，写入内容
+//        Paragraph paragraph = new Paragraph("hello world", fontChinese);
+//        document.add(paragraph);
+//        //第五步，关闭文档
+//        document.close();
+//    }
+}
